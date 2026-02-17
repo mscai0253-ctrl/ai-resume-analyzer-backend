3584ca2fbe544f78bff71e0736753d65ffe7785c
Avdhut <mscai0253@gmail.com>
2026-02-17 18:33:44 +0530

    fix: nltk download for render

--- backend/extractor.py
+++ backend/extractor.py
@@ -1,3 +1,14 @@
+import nltk
+import os
+
+nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
+os.makedirs(nltk_data_path, exist_ok=True)
+
+nltk.download("punkt", download_dir=nltk_data_path)
+nltk.download("stopwords", download_dir=nltk_data_path)
+
+nltk.data.path.append(nltk_data_path)
+
 import re 
 from skills import SKILLS
 from nltk.corpus import stopwords
